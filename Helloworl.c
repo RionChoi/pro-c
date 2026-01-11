f8dbcf87a3dddfb8c26feed8d4bdea74599b006c
ekko68 <haijun82923@gmail.com>
2026-01-11 15:43:37 +0900

    first commit hello world

---
+++ Helloworl.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 프로그램 시작
+    printf("Hello, World!\n");
+    int age = 12;
+    printf("나이는 %d살입니다.\n", age);
+    age += 1;
+    printf("내년에는 %d살입니다.\n", age);
+    /* 프로그램 종료 */
+    /* 주석 */
+    printf("프로그램을 종료합니다.\n");
+    return 0;
+}
